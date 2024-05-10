1260849a69a40973809b2ca965e0447ced12481a
nizam elahi <nizamelahi@gmail.com>
2024-05-11 02:31:21 +0500

    minor

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
 export const AI_NAME = "Medicare Advantage";
-export const AI_DESCRIPTION = "A comprehensive guide for your Medicare Advantage plans";
+export const AI_DESCRIPTION = "A comprehensive guide for your Medicare Advantage plans.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
