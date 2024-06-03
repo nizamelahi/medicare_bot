62b6392416e711c51e19be43cbbb0aa896f5b98a
nizam elahi <nizamelahi@gmail.com>
2024-06-03 20:19:59 +0500

    minor

--- src/features/chat-page/chat-services/chat-api/chat-api-plans-extension.ts
+++ src/features/chat-page/chat-services/chat-api/chat-api-plans-extension.ts
@@ -46,13 +46,19 @@ export const apicall = async (req: Request) => {
 
   // plan_entity api
   const body = await req.json();
+  if (body.plan_id)
+    var plan_id=body.plan_id
+  // else
+  // { 
+  //   plan_id=body["message"]["functionCall"]["parameters"]
+  // }
 
   console.log(`plan_id : ${body.plan_id}`)
 
-  console.log(body)
+  console.log(body["message"]["functionCall"]["parameters"])
 
   if (body.plan_id) {
-    var api_url = new URL(`${process.env.PLAN_ENTITY_URL!}${body.plan_id}`);
+    var api_url = new URL(`${process.env.PLAN_ENTITY_URL!}${plan_id}`);
 
     var api_req_start_time = Date.now()
     const response = await fetch(api_url, {
